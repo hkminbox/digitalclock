5ee481c83174015efa26331e757ff90fcc75301d
hkminbox <62156241+hkminbox@users.noreply.github.com>
2020-05-24 10:19:38 +0530

    Changes to clock.js
    Added month, year, day variables(yet to be displayed).
    Added logs.

--- clock.js
+++ clock.js
@@ -1,14 +1,18 @@
-var flags = false;
+var flags = true;
 if(flags === true)
 {
 	var CHECK_LOG =true;
+	var CHECK_MONTH =true;
 	var SECOND_DELAY =true;
 	var CHECK_LOG_DISPLAY = true;
 	var SECOND_DELAY_DISPLAY =true;
-}
+	var DAY_DELAY = true;
 
+}
+monthName = ["January", "February", "March", "April", "May", "June","July", "August", "September", "October", "November", "December"];
 var secHistory;
 var secHistory2;
+var dayHistory;
 
 
 function setup()
@@ -25,10 +29,31 @@ function draw()
 	let hr = hour();
 	let min = minute();
 	let sec = second();
+	let d = day();
+	let m = month();
+	let y = year();
+
+	m = monthName[m-1];
+
+
+	if(CHECK_MONTH && DAY_DELAY)
+	{
+		console.log(d+ ' : ' + m + ' : ' + y);
+		dayHistory = d;
+		DAY_DELAY = false;
+	}
+	if(d != dayHistory)
+	{
+		DAY_DELAY = true;
+
+	}
+
+
+
 	if(CHECK_LOG && SECOND_DELAY)
 	{
 		console.log((hr%12)+ ' : ' + min + ' : ' + sec);
-		secHistory = sec
+		secHistory = sec;
 		SECOND_DELAY = false;
 	}
 	if(sec != secHistory)
